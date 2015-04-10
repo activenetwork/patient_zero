0371fe94bbe4f58fc75c75b1c97a03343355dbce
Devin Clark <notdevinclark@gmail.com>
2015-04-10 14:11:54 -0500

    updates tests for authorization class

--- spec/patient_zero/authorization_spec.rb
+++ spec/patient_zero/authorization_spec.rb
@@ -4,14 +4,22 @@ module PatientZero
   describe Authorization do
     describe '.token' do
       let(:token) { 'token-shmoken' }
-      let(:body) { "{\"status\":200,\"error\":null,\"user_token\":\"#{token}\"}" }
-      before{ allow(Base.connection).to receive(:post).and_return double(body: body) }
+      let(:authorization_response) { response_with_body user_token: token }
+      before{ allow(Base.connection).to receive(:post).with('/mobile/api/v1/user/login', anything).and_return authorization_response }
       it 'calls the login api endpoint' do
         expect(Base.connection).to receive(:post).with('/mobile/api/v1/user/login', anything)
         Authorization.token
       end
-      it 'returns a user_token' do
-        expect(Authorization.token).to eq token
+      context 'when email and password are correct' do
+        it 'returns a user_token' do
+          expect(Authorization.token).to eq token
+        end
+      end
+      context 'when email and password are incorrect' do
+        let(:authorization_response) { response_with_body error: 'Invalid login/password' }
+        it 'raises a PatientZero::Error error' do
+          expect{ Authorization.token }.to raise_error PatientZero::Error
+        end
       end
     end
   end
