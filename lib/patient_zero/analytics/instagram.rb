6417d27b094247edd4e69295082876a35069292a
Devin Clark <notdevinclark@gmail.com>
2015-04-13 15:07:01 -0500

    updates to analytics stuff
    updates to Instagram and Twitter analytic accessors

--- lib/patient_zero/analytics/instagram.rb
+++ lib/patient_zero/analytics/instagram.rb
@@ -2,23 +2,23 @@ module PatientZero
   module Analytics
     class Instagram < Base
       def posts
-        @posts ||= analytical_data[:total_posts]
+        analytical_data[:total_posts]
       end
 
       def likes
-        @likes ||= analytical_data[:total_likes]
+        analytical_data[:total_likes]
       end
 
       def comments
-        @comments ||= analytical_data[:total_comments]
+        analytical_data[:total_comments]
       end
 
       def followers
-        @followers ||= analytical_data[:total_followers]
+        analytical_data[:total_followers]
       end
 
       def following
-        @following ||= analytical_data[:total_following]
+        analytical_data[:total_following]
       end
     end
   end
--- lib/patient_zero/analytics/twitter.rb
+++ lib/patient_zero/analytics/twitter.rb
@@ -1,7 +1,51 @@
 module PatientZero
   module Analytics
     class Twitter < Base
+      def impressions
+      end
 
+      def reach
+        followers
+      end
+
+      def engagements
+      end
+
+      def tweets
+        analytical_data[:total_tweets]
+      end
+
+      def retweets
+        analytical_data[:total_retweets]
+      end
+
+      def tweets_with_links
+        analytical_data[:total_tweets_with_links]
+      end
+
+      def followers
+        analytical_data[:total_tweets]
+      end
+
+      def following
+        analytical_data[:total_tweets]
+      end
+
+      def lists
+        analytical_data[:total_lists]
+      end
+
+      def favorites
+        analytical_data[:total_favorites]
+      end
+
+      def user_mentions
+        analytical_data[:total_user_mentions]
+      end
+
+      def impressions
+        analytical_data[:total_impressions]
+      end
     end
   end
 end
