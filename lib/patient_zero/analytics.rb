78a8634caf709fabe527b82a501f550b10b2cfb1
Devin Clark <notdevinclark@gmail.com>
2015-05-06 10:45:36 -0500

    adds tumblr to the analytics module

--- lib/patient_zero/analytics.rb
+++ lib/patient_zero/analytics.rb
@@ -2,12 +2,14 @@ require 'patient_zero/analytics/base'
 require 'patient_zero/analytics/twitter'
 require 'patient_zero/analytics/facebook'
 require 'patient_zero/analytics/instagram'
+require 'patient_zero/analytics/tumblr'
 
 module PatientZero
   module Analytics
     SOURCE_TYPES = {'twitter' => Twitter,
                     'facebook' => Facebook,
-                    'instagram' => Instagram}
+                    'instagram' => Instagram,
+                    'tumblr' => Tumblr}
 
     def self.for_platform platform, params={}
       SOURCE_TYPES[platform].new params
